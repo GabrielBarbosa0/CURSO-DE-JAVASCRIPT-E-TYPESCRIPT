bc4ef5d2afc863d2d50d7d16f91ec761fd24c891
GabrielBarbosa0 <82109149+GabrielBarbosa0@users.noreply.github.com>
2023-01-13 13:38:17 -0300

    commit

---
+++ aula3/indx.js
@@ -0,0 +1,9 @@
+console.log('Gabriel Barbosa'); //string = texto
+console.log("Gabriel Barbosa");
+console.log(`Gabriel Barbosa`);
+
+console.log("'Gabriel Barbosa'");
+console.log(`'Gabriel' "Barbosa"`);
+
+
+console.log(69, 81.7, 'Gabriel de novo') //number = número
