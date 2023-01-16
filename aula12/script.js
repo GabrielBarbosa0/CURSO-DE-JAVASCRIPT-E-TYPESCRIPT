6c7122f5d56e9881cc560c609c1325ac0d5fa01d
GabrielBarbosa0 <82109149+GabrielBarbosa0@users.noreply.github.com>
2023-01-16 12:01:35 -0300

    commit

---
+++ aula12/script.js
@@ -0,0 +1,15 @@
+let varA = 'A'; //B 
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+// const varD = varA;
+// let varD = null;
+// varD = varA
+
+[varA, varB, varC] = [varB, varC, varA]
+
+// varA = varB
+// varB = varC
+// varC = varD
+
+console.log(varA, varB, varC)
