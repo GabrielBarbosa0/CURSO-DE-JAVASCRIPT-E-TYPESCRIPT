7a5d7a1208642c3875348618fac3f0b709504155
GabrielBarbosa0 <82109149+GabrielBarbosa0@users.noreply.github.com>
2023-01-18 14:12:12 -0300

    commit

---
+++ aula13/script.js
@@ -0,0 +1,14 @@
+//               01234567
+let umaString = "Um texto";
+let outraString = "O rato roeu a roupa do rei de roma";
+
+console.log(umaString[7])
+
+console.log(umaString.concat(' ', 'em', ' ', 'um'));
+
+console.log(umaString.indexOf('texto'));
+
+console.log(umaString.replace(/Um/, 'Outro'));
+
+console.log(outraString.replace(/r/g, '#'));
+
