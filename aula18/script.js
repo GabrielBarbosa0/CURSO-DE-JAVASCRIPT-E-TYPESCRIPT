12c68e1babf1b24c1624bf51db39f23f356aa927
GabrielBarbosa0 <82109149+GabrielBarbosa0@users.noreply.github.com>
2023-11-26 12:18:13 -0300

    commit

---
+++ aula18/script.js
@@ -0,0 +1,30 @@
+// const array = [4,5,6];
+
+// const pessoa = {
+//     nome: 'Luiz',
+//     sobrenome: 'Barbosa',
+//     idade: 25
+// };
+
+
+// function criaPessoa (nome, sobrenome, idade) {
+//     return {
+//         nome,
+//         sobrenome,
+//         idade
+//     };
+// }
+
+// const pessoa1 = criaPessoa('Gabriel', 'Barbosa', 18)
+// const pessoa2 = criaPessoa('Danilo', 'Vadinho', 33)
+// const pessoa3  = criaPessoa('Roger', 'Pinto', 58)
+
+// console.log(pessoa1.nome);
+// console.log(pessoa2.nome);
+// console.log(pessoa3.nome);
+
+const pessoa1 = {
+    nome: 'Nivaldo',
+    sobrenome: 'Batista',
+    idade: 35,
+};
